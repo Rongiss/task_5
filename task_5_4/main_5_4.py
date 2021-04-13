85d3034a8502ca41f68799c7cf9465d1b34af6ff
romangrigorev <rongiss@mail.ru>
2021-04-13 22:46:21 +0800

    12

---
+++ task_5_4/main_5_4.py
@@ -0,0 +1,10 @@
+
+def main_for_car():
+    # Затраты на масло в месяц
+    oil_foк_car = float(input('Затраты на масло для машины в месяц - $'))
+    credit_car = float(input('Оплата кредите за машину - $'))
+    gas_for_car = float(input('Затраты на топливо в месяц - $'))
+    tires_for_car = float(input('Затраты на шины в месяц - $'))
+    car_insurance = float(input('Затраты на страхоку в месяц - $'))
+    maintenance = float(input('Затраты на техлбслуживание - $'))
+
