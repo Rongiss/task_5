f0c2d813ef8c4a9a30292a54e5616326878aa443
r.grigoriev <rongis03@gmail.com>
2021-04-13 16:17:55 +0800

    123

--- task_5_2/calkulate_and_out_task_5_2.py
+++ task_5_2/calkulate_and_out_task_5_2.py
@@ -2,9 +2,14 @@ import math
 # Функция расчета и вывода данных
 
 def calcalculete_and_out(fed_tax, reg_tax, cash):
+
+    # Приобразование процентной ставки
     fed_tax /= 100
+    # Приобразование процентной ставки
     reg_tax /= 100
+    # Расчет суммы федерального налога
     cash_fed_tax = cash * fed_tax
+    # Расчет суммы регионального налога
     cash_reg_tax = cash * reg_tax
 
     print('{0:_^45}'.format(''))
--- task_5_2/main_task_5_2.py
+++ task_5_2/main_task_5_2.py
@@ -12,8 +12,14 @@ def main():
     # Налоговых ставок
     intro_task_5_2.info(FEDERATION_TAX, REGIONAL_TAX)
 
+    # Функция ввода данных
     money_from_user = input_task_5_2.enter_cash()
+
+    # Функция расчета суму
     calkulate_and_out_task_5_2.calcalculete_and_out(FEDERATION_TAX, REGIONAL_TAX, money_from_user)
+
+    # Функция для вывода завершающего сообщения
     intro_task_5_2.out_list()
 
+# Вызов основной функции
 main()
