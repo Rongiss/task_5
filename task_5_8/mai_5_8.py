4a5555e26c34f771c1168ccfe967bb291f5e737e
r.grigoriev <rongis03@gmail.com>
2021-04-16 16:28:14 +0800

    Дописать коментарии в коде

--- task_5_8/mai_5_8.py
+++ task_5_8/mai_5_8.py
@@ -5,10 +5,14 @@ CONST_WORK_TIME = 8
 PRICE_COMPANY = 2000
 
 def main():
-    color_price = input_price_color()
-    area_working = input_area_for_work()
 
+    bank, work_time, price_color, price_work, total_price = calculate(input_area_for_work(),input_price_color())
 
+    print('\nКоличество емкостей краски составляет - ', bank)
+    print('Время работы составит - ', work_time)
+    print('Цена краски равна - ', price_color)
+    print('Стоимость работ составляет', price_work)
+    print('Итоговая сумма равна - ', total_price)
 
 def input_area_for_work():
     print('\nВведите площадь для  покраски')
@@ -28,6 +32,25 @@ def input_price_color():
         price_color = float(input('Сумма составляет - '))
     return price_color
 
+def calculate(area, price_color):
 
+    # Вычисляю количество емкостей
+    band = area / CONST_AREA
+    if band % 1 > 0:
+        band = area // CONST_AREA + 1
+
+    # Часы работы
+    time_work = area / CONST_AREA * CONST_WORK_TIME
+
+    # Стоимость краски
+    color_money = band * price_color
+
+    # Стоисочть работ
+    money_work = time_work * PRICE_COMPANY
+
+    # Общая стоимость
+    total_sum = money_work + color_money
+    # Возвращау значение
+    return band, time_work, color_money, money_work, total_sum
 
 main()
