e6f0a70607011e9abcf701f745d7f04b9fc6c9ff
Roman <rongiss@mail.ru>
2021-04-23 16:32:05 +0800

    5_25 done..

--- task_5_25/main_5 _25.py
+++ task_5_25/main_5 _25.py
@@ -3,7 +3,7 @@ import turtle
 x = 0
 y = 0
 step = 8
-long = 100
+long = 200
 step_long = long / step
 long_gip = (long ** 2 + long ** 2) ** 0.5
 
@@ -11,6 +11,7 @@ def main():
     sqare(x, y, step)
     line_and_gip(x, y, long, long_gip)
     second_sqare(x, y, step, long)
+    third_sqare(x, y, step, long)
 
 def line_and_gip(x, y, long, long_gip):
     turtle.penup()
@@ -63,6 +64,21 @@ def second_sqare(x,y,step, long):
         turtle.forward(long - step_long * 2)
         turtle.left(90)
         sq += 1
+
+def third_sqare(x,y,step, long):
+    sq = 0
+    turtle.fillcolor('black')
+    turtle.penup()
+    turtle.goto(step_long * 2, step_long * 2)
+    turtle.pendown()
+
+    turtle.setheading(0)
+    turtle.begin_fill()
+    while sq < 4:
+        turtle.forward(long - step_long * 4)
+        turtle.left(90)
+        sq += 1
+    turtle.end_fill()
 
 main()
 
